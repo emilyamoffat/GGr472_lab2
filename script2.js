037beac028cc78f684694f8cef28cbd7b08ce4d3
emilyamoffat <emilyamoffat@gmail.com>
2025-02-10 16:48:20 -0500

    Updates to lab 2 code

--- script2.js
+++ script2.js
@@ -1,26 +1,50 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiZW1pbHlhbW9mZmF0IiwiYSI6ImNtNmI0d3puaTA0dG0yam84dzNiZTQ5NjIifQ.A1PSOyaJV6TF-lKcIFMHQA'; // Add default public map token from your Mapbox account
 const map = new mapboxgl.Map({
 container: 'my-map', // map container ID
-style: 'mapbox://styles/mapbox/satellite-streets-v12', // style URL
+style: 'mapbox://styles/emilyamoffat/cm6zhee9000s201sbc2m0g0xc', // style URL
 center: [-79.3832, 43.6532], // starting position [lng, lat]
 zoom: 9, // starting zoom level
 });
 
-//intended bounds - -79.62,43.6,-79.12,43.83
-// Add a data source from a GeoJSON file
-map.addSource('buildings-data', {
-    type: 'geojson',
-    data: '...' // Your URL to your buildings.geojson file
-    });
-    map.addLayer({
-    'id': 'buildings-point',
-    'type': 'circle',
-    'source': 'buildings-data',
-    'paint': {
-    'circle-radius': 5,
-    'circle-color': '#007cbf'
-    }
+//Toronto bounds\
+map.fitBounds([
+    [-79.62, 43.6], // Southwest corner
+    [-79.12, 43.83] // Northeast corner
+]);
+
+// Add layers to mapbox basemap
+map.on('load', function() {
+
+    // Add parks data - vector line tileset housed in mapbox
+    map.addSource('parks-data', { 
+        'type': 'vector',
+        'url': 'mapbox://emilyamoffat.1ndjd2c9' // Tileset ID
     });
 
-    C:\Users\emily\OneDrive\Documents\GitHub\GGr472_lab2\script2.js
-    
+    map.addLayer({
+        'id': 'parks-line',
+        'type': 'line',
+        'source': 'parks-data', 
+        'paint': {
+            'line-color': '#556b2f',
+            'line-width': 2
+        },
+        'source-layer': 'ON_prr-4eotyv' // Mapbox tileset source layer name
+    });
+
+    // Add Toronto Public Water Fountains point data from GeoJSON file, housed in GitHub
+    map.addSource('fountains-data', {
+        type: 'geojson',
+        data: 'https://raw.githubusercontent.com/emilyamoffat/GGr472_lab2/main/Parks%20Drinking%20Fountains%20-%204326.geojson'
+    });
+
+    map.addLayer({
+        'id': 'fountains-point',
+        'type': 'circle',
+        'source': 'fountains-data',
+        'paint': {
+            'circle-radius': 2.5,
+            'circle-color': '#6495ed'
+        }
+    });
+});
